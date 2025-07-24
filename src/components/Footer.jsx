9a141f1effe2bd500f537768de4898893382f35f
Manto Ariyansyah <mantoariyansyah23@gmail.com>
2025-07-24 07:18:41 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 // Import gambar
-import logo from "../assets/logo.png";
+import logo from "/src/assets/logo.png";
 import twett from "../assets/img/twett.png";
 import fb from "../assets/img/fb.png";
 import instagram from "../assets/img/instagram.png";
