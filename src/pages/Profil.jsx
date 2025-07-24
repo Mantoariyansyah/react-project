0b2de2ecad5a4dfe4b75b042ce5416cd12e921c8
Manto Ariyansyah <mantoariyansyah23@gmail.com>
2025-07-24 08:00:31 +0700

    update

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -4,7 +4,7 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-
+import fotged from "../img/fotogedungsekolah.png";
 
 import gedung from "../img/gambar15.png";
 import aula from "../img/gambar16.png";
@@ -127,7 +127,7 @@ function Profil() {
                     SMAS KRISTEN BETHEL JAKARTA
                 </h1>
                 <div className="flex items-center justify-center">
-                    <img src="/src/img/fotogedungsekolah.png" alt="" className="w-[80%]" />
+                    <img src={fotged} alt="" className="w-[80%]" />
                 </div>
                 <div className="px-[123px] pt-14 pb-10 space-y-8">
                     <p className="text-sm font-normal leading-snug tracking-wider">SMAS Kristen Bethel Jakarta adalah sebuah sekolah menengah atas swasta Kristen yang berlokasi di kawasan strategis di Petamburan, Jakarta Pusat. Sekolah ini berdiri dengan tujuan mulia untuk menjadi pusat pendidikan yang tidak hanya mengedepankan prestasi akademik, tetapi juga membentuk karakter siswa yang beriman, berintegritas, dan melayani sesuai dengan nilai-nilai Kristiani.</p>
